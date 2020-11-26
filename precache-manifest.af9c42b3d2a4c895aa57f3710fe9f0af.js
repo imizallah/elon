c0382175532eac28a685b68e043c18b401e6219c
imizallah <imizallah@gmail.com>
2020-11-27 00:31:15 +0100

    Updates

---
+++ precache-manifest.af9c42b3d2a4c895aa57f3710fe9f0af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ad91d8d1422ff8a483900957fb0ac6",
+    "url": "/elon/index.html"
+  },
+  {
+    "revision": "8a757dab3daec56647b2",
+    "url": "/elon/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "957768dd2a6edb85b2cd",
+    "url": "/elon/static/css/main.73178c5e.chunk.css"
+  },
+  {
+    "revision": "8a757dab3daec56647b2",
+    "url": "/elon/static/js/2.422beba6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/elon/static/js/2.422beba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "957768dd2a6edb85b2cd",
+    "url": "/elon/static/js/main.7e0ee810.chunk.js"
+  },
+  {
+    "revision": "885daf5521ffd9ac24f9",
+    "url": "/elon/static/js/runtime-main.075a98da.js"
+  }
+]);
